4b29b4d9dcb2d8373f7d39c3d4328a7b9e7b803d
YuanDeshan <1737671795@qq.com>
2019-09-12 20:33:48 +0800

    update

--- c programing design(tanhaoqiang).c
+++ c programing design(tanhaoqiang).c
@@ -80,28 +80,7 @@ int main(){//
 
 
 
-	/*int i, j,n;
-	for (i = 1; i < 5; i++){
-	
-		for (j = 1; j <=5- i; j++){
-			printf(" ");
-		}
-		for (j = 1; j < (2*i); j++){
-			printf("*");
-		}
-		printf("\n");
-	}
-	for (i = 1; i < 4; i++){
-		for (j = 1; j <= i; j++){
-			printf(" ");
-		}
-		for (j = 1; j < 7- 2*i; j++){
-			printf("*");
-		}
-		printf("\n");
 
-	}
-*/
 	/*int sum=1, i;
 	for (i = 0; i < 9; i++){
 		sum =(sum+1)* 2 ;
